62d1ba8c95cdf6888bdc44e87fc1ebd7cb9d481c
Madiapo <letsoaloantonia@gmail.com>
2022-03-26 06:45:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *main function for printing
- *
+ *main function for printing the messagege
  * return 0 after printing the message
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
