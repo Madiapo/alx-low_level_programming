12c7837616a0d6ab1c099e13984546cf409c915f
Madiapo <letsoaloantonia@gmail.com>
2022-03-26 07:26:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  *
  *return: 0 after printing the function
 */
+/**
+ * main_function - this is a description
+ */
 int main(void)
-	/**
-	 * main function
-	 */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
